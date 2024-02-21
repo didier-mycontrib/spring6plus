17d25e9f3dd19cf8ab0a5268de9e406db3fc7e85
DidierDefrance <didier@d-defrance.fr>
2024-02-21 11:34:00 +0100

    json ok

--- mySpringBatch/src/main/java/tp/mySpringBatch/MySpringBatchApplication.java
+++ mySpringBatch/src/main/java/tp/mySpringBatch/MySpringBatchApplication.java
@@ -35,9 +35,11 @@ public class MySpringBatchApplication implements CommandLineRunner {
 	//Job job = (Job) applicationContext.getBean("myHelloWorldJob");
     //Job job = (Job) applicationContext.getBean("copyFromCsvToCsvJob");
     //Job job = (Job) applicationContext.getBean("fromCsvToXmlJob");
+	// Job job = (Job) applicationContext.getBean("fromCsvToJsonJob");
 	// Job job = (Job) applicationContext.getBean("fromXmlToCsvJob");
+	  Job job = (Job) applicationContext.getBean("fromJsonToXmlJob");
     //Job job = (Job) applicationContext.getBean("insertIntoDbFromCsvJob");
-	Job job = (Job) applicationContext.getBean("insertIntoCsvFromDbJob");
+	//Job job = (Job) applicationContext.getBean("insertIntoCsvFromDbJob");
 	//Job job = (Job) applicationContext.getBean("withMyTaskletJob");
 
     JobParameters jobParameters = new JobParametersBuilder()
---
+++ mySpringBatch/src/main/java/tp/mySpringBatch/writer/MyJsonFilePersonWriterConfig.java
@@ -0,0 +1,32 @@
+package tp.mySpringBatch.writer;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
+import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.WritableResource;
+
+import tp.mySpringBatch.model.Person;
+
+@Configuration
+public class MyJsonFilePersonWriterConfig {
+
+
+	  @Value("file:data/output/json/outputData.json") //to read in project root directory
+	  //NB: by default @Value(path) is @Value("classpath:path) //to read in src/main/resource or other classpath part
+	  private WritableResource outputJsonResource;
+	 
+	  
+	  //avec builder:
+	  @Bean @Qualifier("json")
+	  ItemWriter<Person> personJsonFileItemWriter() {		  
+		  return new JsonFileItemWriterBuilder<Person>()
+				     .name("personJsonFileItemWriter")
+	                 .jsonObjectMarshaller(new JacksonJsonObjectMarshaller<>())
+	                 .resource(outputJsonResource)
+	                 .build();
+		}
+}

e9e641811d4a080f9f721f3f179a1045c1219600
DidierDefrance <didier@d-defrance.fr>
2024-03-11 17:54:59 +0100

    s

--- my-native-app/build_native_image.sh
+++ my-native-app/build_native_image.sh
@@ -1,1 +1,2 @@
-mvn -Pnative clean spring-boot:build-image > resBuild.txt 2>&1
+mvn -DskipTests=true -Pnative clean spring-boot:build-image > resBuild.txt 2>&1
+#docker image rm my-native-app:0.0.1-SNAPSHOT
